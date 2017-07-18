ffbf10b2b50f345d1f49a27d33e9786478a9db43
Luis Pedro Coelho <luis@luispedro.org>
2017-07-18 21:57:29 +0200

    BLD Update nix script to newer nixpkgs

--- default.nix
+++ default.nix
@@ -2,44 +2,22 @@
 with import <nixpkgs> {};
 
 let
-    envname = "pyintro-talk";
-    python = python27Full;
-    pyp = pkgs.python27Packages;
+    p = python27.withPackages (ps: with ps;
+          [
+            jupyter_client
+            jupyter_console
+            notebook
+            ipywidgets
+            ipykernel
+            ipython
+
+
+            seaborn
+            matplotlib
+
+            numpy
+            scipy
+          ]);
 in
-
-buildPythonPackage { 
-  name = "${envname}-env";
-  buildInputs = [
-     python
-     cacert
-    ];
-   pythonPath = with pyp; [
-    jupyter_client
-    jupyter_console
-    notebook
-    ipywidgets
-    ipykernel
-    ipython
-
-    seaborn
-    matplotlib
-
-    numpy
-    scipy
-    pyside
-    sqlite3
-    readline
-    numpydoc
-    virtualenv
-  ];
-  src = null;
-  # When used as `nix-shell --pure`
-  shellHook = ''
-  export NIX_ENV="[${envname}] "
-  '';
-  # used when building environments
-  extraCmds = ''
-  export NIX_ENV="[${envname}] "
-  '';
-}
-
+# We need ignoreCollisions because of https://github.com/NixOS/nixpkgs/issues/23855
+(p.override { ignoreCollisions = true; }).env
