f3ec1d9abce6a195803924c5881c7cf5011dcca4
Luis Pedro Coelho <luis@luispedro.org>
2019-07-03 21:38:25 +0200

    BLD Update nix build script to work with newer nix-shell

--- default.nix
+++ default.nix
@@ -1,23 +1,32 @@
-#!/usr/bin/env nix-shell
-with import <nixpkgs> {};
+
+with (import (builtins.fetchTarball {
+  name = "nixml-stable-19.04";
+  url = https://github.com/nixos/nixpkgs/archive/37694c8cc0e9ecab60d06f1d9a2fd0073bcc5fa3.tar.gz;
+  sha256 = "1ibjg6ln2y764wfg0yd0v055jm4xzhmxy5lfz63m6jr3y16jdmzb";
+}) {});
 
 let
-    p = python36.withPackages (ps: with ps;
-          [
-            jupyter_client
-            jupyter_console
-            notebook
-            ipywidgets
-            ipykernel
-            ipython
+  pwp = python36.buildEnv.override {
+    extraLibs = (with python36Packages; [
+        jupyter_client
+    jupyter_console
+    notebook
+    ipywidgets
+    ipykernel
+    ipython
+    seaborn
+    matplotlib
+    numpy
+    scipy
+    ]);
+    ignoreCollisions = true;
+ };
 
-
-            seaborn
-            matplotlib
-
-            numpy
-            scipy
-          ]);
 in
-# We need ignoreCollisions because of https://github.com/NixOS/nixpkgs/issues/23855
-(p.override { ignoreCollisions = true; }).env
+
+stdenv.mkDerivation {
+  name = "pynix-env";
+  buildInputs = [
+    pwp
+  ];
+}
