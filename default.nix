e4c7056fb01d5b222fb6e98cb2e3f4434c11a3c5
Luis Pedro Coelho <luis@luispedro.org>
2020-07-13 19:19:03 +0200

    BLD+MIN Update to newer nix + newer Python

--- default.nix
+++ default.nix
@@ -1,23 +1,24 @@
 
 with (import (builtins.fetchTarball {
-  name = "nixml-stable-19.04";
-  url = https://github.com/nixos/nixpkgs/archive/37694c8cc0e9ecab60d06f1d9a2fd0073bcc5fa3.tar.gz;
-  sha256 = "1ibjg6ln2y764wfg0yd0v055jm4xzhmxy5lfz63m6jr3y16jdmzb";
+  name = "nixml-stable-20.07";
+  url = https://github.com/nixos/nixpkgs/archive/a1a8e7b0217fd6a72a5d008d8dfb3fdf8ba92e00.tar.gz;
+  sha256 = "18xna03j2vp551f2x80w7y02865hvpmrppp5i6mxp2g36bldym6h";
 }) {});
 
 let
-  pwp = python36.buildEnv.override {
-    extraLibs = (with python36Packages; [
+  pwp = python37.buildEnv.override {
+    extraLibs = (with python37Packages; [
         jupyter_client
-    jupyter_console
-    notebook
-    ipywidgets
-    ipykernel
-    ipython
-    seaborn
-    matplotlib
-    numpy
-    scipy
+        jupyter_console
+        notebook
+        ipywidgets
+        ipykernel
+        nbformat
+        ipython
+        seaborn
+        matplotlib
+        numpy
+        scipy
     ]);
     ignoreCollisions = true;
  };
