f4f8410ff12175a04a9b562fa797093facad8548
Luis Pedro Coelho <luis@luispedro.org>
2022-07-24 09:58:51 +0200

    MIN Update nixpkgs pin

--- default.nix
+++ default.nix
@@ -1,24 +1,23 @@
 
 with (import (builtins.fetchTarball {
-  name = "nixml-stable-20.07";
-  url = https://github.com/nixos/nixpkgs/archive/a1a8e7b0217fd6a72a5d008d8dfb3fdf8ba92e00.tar.gz;
-  sha256 = "18xna03j2vp551f2x80w7y02865hvpmrppp5i6mxp2g36bldym6h";
+  name = "nixml-stable-22.05";
+  url = https://github.com/nixos/nixpkgs/archive/5c0c0f0ee8d7b536925580e723a5f03bf6e0b7a4.tar.gz;
+  sha256 = "0v1fbc261by0dfdwysr906garc081swwlyy5x3d7daf98gr5k7yx";
 }) {});
 
 let
-  pwp = python37.buildEnv.override {
-    extraLibs = (with python37Packages; [
+  pwp = python39.buildEnv.override {
+    extraLibs = (with python39Packages; [
         jupyter_client
-        jupyter_console
-        notebook
-        ipywidgets
-        ipykernel
-        nbformat
-        ipython
-        seaborn
-        matplotlib
-        numpy
-        scipy
+    jupyter_console
+    notebook
+    ipywidgets
+    ipykernel
+    ipython
+    seaborn
+    matplotlib
+    numpy
+    scipy
     ]);
     ignoreCollisions = true;
  };
@@ -28,6 +27,6 @@ in
 stdenv.mkDerivation {
   name = "pynix-env";
   buildInputs = [
-    pwp
+    pwp vim
   ];
 }
